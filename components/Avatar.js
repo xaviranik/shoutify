e6831d7866aa7e680147762f10492fd2d6c31bd7
Zabir Anik <xaviranik.me@gmail.com>
2021-12-25 20:56:05 +0600

    add: size support for avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,12 +1,15 @@
-const Avatar = ({ customClass, src }) => {
+const Avatar = ({ customClass, src, size, alt }) => {
+  let dimensions = size ? `w-${size} h-${size}` : "w-8 h-8";
+
   return (
-    <div>
-      <img
-        className={`h-8 rounded-full cursor-pointer ${customClass}`}
-        src={src ?? `https://i.pravatar.cc/150?img=60`}
-        alt="Avatar"
-      />
-    </div>
+    <img
+      loading="lazy"
+      className={`${dimensions} object-cover rounded-full cursor-pointer ${
+        customClass ?? ""
+      }`}
+      src={src ?? `https://i.pravatar.cc/150?img=60`}
+      alt={alt ?? "avatar"}
+    />
   );
 };
 
